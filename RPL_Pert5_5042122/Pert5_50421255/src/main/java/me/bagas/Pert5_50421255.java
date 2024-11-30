c3c86417614d5aa71590db94ebf2492074d57a35
jayamahe11 <152406345+jayamahe11@users.noreply.github.com>
2024-11-30 20:09:48 +0700

    Add files via upload

---
+++ RPL_Pert5_5042122/Pert5_50421255/src/main/java/me/bagas/Pert5_50421255.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.bagas;
+
+import me.bagas.controller.MahasiswaController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class Pert5_50421255 implements CommandLineRunner{
+    @Autowired
+    private MahasiswaController mhsController;
+    public static void main(String[] args) {
+        SpringApplication.run(Pert5_50421255.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+        mhsController.tampilkanMenu();
+    }
+}
